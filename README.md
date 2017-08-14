7421e2c23dc2ad77cb09f6ce2736ef9abb039aa1
Jose Brandao <jose.brandao@blip.pt>
2017-08-14 16:10:22 +0100

    added snippet

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
-# headlessChromeMatchMedia
-How to fully emulate a mobile device with Headless Chrome(match media queries included).
+# Match Media queries working with Headless Chrome
+
+**A code snippet that shows how to fully emulate a mobile device with Headless Chrom via node(match media queries included).**
+
+When playing around with headless browsers I come to the fact that it's hard to find a solution where I could completely emulate a mobile device due to match media not working properly.
+
+Until now and there is not much documentation about Headless Chrome.
+
+After some time struggling, I found a way to make Headless Chrome to respect match media queries.
+
+This example complitely emulates an iPhone6 Plus, making match media queries working properly.
+
+## Setup and run
+
+Install the node modules required to run this sript:
+
+`
+npm install chrome-remote-interface
+`
+
+`
+npm install chrome-launcher
+`
+
+
+Run it with node:
+
+`
+node index.js
+`
+
+___
+
+Many thanks to this [tutorial](https://www.sitepoint.com/headless-chrome-node-js/) for providing a simple way to use Headless Chrome.
+
+
+
+
---
+++ index.js
@@ -0,0 +1,61 @@
+const chromeLauncher = require('chrome-launcher');
+const CDP = require('chrome-remote-interface');
+const file = require('fs');
+
+const viewport = [414,736];
+
+(async function () {
+    async function launchChrome() {
+        return await chromeLauncher.launch({
+            chromeFlags: [
+                '--disable-gpu',
+                '--headless'
+            ]
+        });
+    }
+    const chrome = await launchChrome();
+    const protocol = await CDP({
+        port: chrome.port
+    });
+
+    const {
+        DOM,
+        Page,
+        Emulation,
+        Runtime,
+        Network
+    } = protocol;
+
+    await Promise.all([Page.enable(), Runtime.enable(), DOM.enable(), Network.enable()]);
+
+    await Emulation.setDeviceMetricsOverride({
+        width: viewport[0], //Set your view port here
+        height: viewport[1],
+        deviceScaleFactor: 0,
+        mobile: true //Identify that you are a mobile phone
+    });
+
+    //Pass a proper user agent
+    Network.setUserAgentOverride({userAgent: 'Mozilla/5.0 (iPhone; CPU iPhone OS 9_1 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Version/9.0 Mobile/13B137 Safari/601.1'});
+
+    Page.navigate({
+        url: 'jose.brandao13.github.io'
+    });
+
+    Page.loadEventFired(async () => {
+        
+        //Let's take a screenshot after a few seconds
+        setTimeout(async function() {
+            const ss = await Page.captureScreenshot({ format: 'png', fromSurface: true});
+            file.writeFile('screenshot.png', ss.data, 'base64', function (err) {
+                if (err) {
+                    console.log(err);
+                }
+            });
+
+            protocol.close();
+            chrome.kill();
+        },5000);
+    });
+
+})();
